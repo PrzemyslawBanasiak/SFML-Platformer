4945a6480f961bf6dae97c8194e652190e409eb1
Banasiak <przemyslaw@banasiak.pl>
2017-11-17 13:45:21 +0100

    Asset Manager

--- Platformer/Managers/AssetManager.cpp
+++ Platformer/Managers/AssetManager.cpp
@@ -1,1 +1,48 @@
 #include "AssetManager.hpp"
+
+void AssetManager::LoadTexture(const std::string& name, const std::string& filePath) {
+    if(_textures.find(name) != _textures.end()) {
+        return;
+    }
+    
+    sf::Texture tex;
+    if(tex.loadFromFile(filePath)) {
+        _textures[name] = tex;
+    }
+}
+
+void AssetManager::LoadAudio(const std::string& name, const std::string& filePath) {
+    if(_sounds.find(name) != _sounds.end()) {
+        return;
+    }
+    
+    sf::SoundBuffer sb;
+    sf::Sound sound;
+    if(sb.loadFromFile(filePath)) {
+        sound.setBuffer(sb);
+        _sounds[name] = sound;
+    }
+}
+
+void AssetManager::LoadFont(const std::string& name, const std::string& filePath) {
+    if(_fonts.find(name) != _fonts.end()) {
+        return;
+    }
+    
+    sf::Font font;
+    if(font.loadFromFile(filePath)) {
+        _fonts[name] = font;
+    }
+}
+
+sf::Texture& AssetManager::GetTexture(const std::string& name) {
+    return _textures.at(name);
+}
+
+sf::Font& AssetManager::GetFont(const std::string& name) {
+    return _fonts.at(name);
+}
+
+sf::Sound& AssetManager::GetAudio(const std::string& name) {
+    return _sounds.at(name);
+}
--- Platformer/Managers/AssetManager.hpp
+++ Platformer/Managers/AssetManager.hpp
@@ -1,5 +1,18 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 class AssetManager {
+public:
+    void LoadTexture(const std::string& name, const std::string& filePath);
+    void LoadAudio(const std::string& name, const std::string& filePath);
+    void LoadFont(const std::string& name, const std::string& filePath);
     
+    sf::Texture& GetTexture(const std::string& name);
+    sf::Font& GetFont(const std::string& name);
+    sf::Sound& GetAudio(const std::string& name);
+private:
+    std::map<std::string, sf::Texture> _textures;
+    std::map<std::string, sf::Sound> _sounds;
+    std::map<std::string, sf::Font> _fonts;
 };
